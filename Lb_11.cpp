806cde93d0bc2e86593e15438b811e6a010ddcf3
Nikita7181 <70635031+Nikita7181@users.noreply.github.com>
2020-12-17 21:59:42 +0300

    Update Lb_11.cpp

--- Lb_11.cpp
+++ Lb_11.cpp
@@ -11,13 +11,13 @@
 std::mutex vector_mtx;
 std::mutex cout_mtx;
 
-struct VectorIndex
+struct VectorIndex// структура, в которой храняться начало и конец вектора
 {
     int start;
     int end;
 };
 
-std::vector<VectorIndex> generate_indexes(std::vector<int> vector, int thread_count)
+std::vector<VectorIndex> generate_indexes(std::vector<int> vector, int thread_count)// функция разделения вектора на потоки(по сути)
 {
     std::vector<VectorIndex> result;
     int slice_size = 0;
@@ -26,7 +26,8 @@ std::vector<VectorIndex> generate_indexes(std::vector<int> vector, int thread_co
         VectorIndex vi;
         vi.start = i * slice_size;
         vi.end = (i + 1) * slice_size;
-        if (i == (thread_count - 1)) {
+        if (i == (thread_count - 1)) 
+        {
             vi.end = vector.size();
         }
         result.push_back(vi);
@@ -34,7 +35,7 @@ std::vector<VectorIndex> generate_indexes(std::vector<int> vector, int thread_co
     return result;
 }
 
-std::vector<int> slicing (std::vector<int>& arr ,VectorIndex vi)
+std::vector<int> slicing (std::vector<int>& arr ,VectorIndex vi)// функция "разрезания" вектора
 {
     auto start = arr.begin() + vi.start;
     auto end = arr.begin() + vi.end + 1;
@@ -43,7 +44,7 @@ std::vector<int> slicing (std::vector<int>& arr ,VectorIndex vi)
     return result;
 }
 
-void find_min(std::vector<int>& vec, VectorIndex vi, int name, int& result)
+void find_min(std::vector<int>& vec, VectorIndex vi, int name, int& result)// функция поиска минимального значения ветора
 {
     vector_mtx.lock();
     std::vector<int> slice = slicing(vec, vi);
@@ -56,7 +57,8 @@ void find_min(std::vector<int>& vec, VectorIndex vi, int name, int& result)
 }
 
 
-std::vector<int> generate_vector(int size) {
+std::vector<int> generate_vector(int size)// функция заполнения вектора
+{
     std::srand(std::time(nullptr));
     std::vector<int> result;
     for(long long int i = 0; i<size; ++i)
@@ -69,7 +71,7 @@ std::vector<int> generate_vector(int size) {
 
 int main()
 {
-    const int thread_count = 1;
+    const int thread_count = 5;// измение кол-во потоков
     int result_min_value;
     std::vector<int> vector;
     std::vector<VectorIndex> indexes;
@@ -82,24 +84,27 @@ int main()
     vector = generate_vector(12600000);
     indexes = generate_indexes(vector, thread_count);
     
-    for (int i = 0; i < thread_count; i++) {
+    for (int i = 0; i < thread_count; i++) 
+    {
         results.push_back(-1);
     }
     
     start_time = std::chrono::steady_clock::now();
     
-    for (int i = 0; i < thread_count; i++) {
+    for (int i = 0; i < thread_count; i++) 
+    {
         threads[i] = new std::thread(find_min, std::ref(vector), indexes[i], i, std::ref(results[i]));
     }
     
     for (auto & thread : threads) {
-        thread->join();
+        thread -> join();
     }
     
     stop_time = std::chrono::steady_clock::now();
     
-    for (int i = 0; i < thread_count; i++) {
-        result_values->push_back(results[i]);
+    for (int i = 0; i < thread_count; i++) 
+    {
+        result_values -> push_back(results[i]);
     }
     
     result_min_value = *std::min_element(result_values->begin(), result_values->end());
@@ -107,3 +112,6 @@ int main()
     std::cout << "time: " << std::chrono::duration_cast<std::chrono::microseconds>(stop_time - start_time).count() << std::endl;
     return 0;
 }
+// В результате можно сказать, что много поток эффективен при 5 потоках
+// при одном потоке скорость 189231 мксек, а при 5 потоках 83224 мксек 
+// самое оптимальное число эллементов 12600000+-500000
