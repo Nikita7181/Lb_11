bb685acda42cec658461f7597c5039259832ee77
Nikita7181 <70635031+Nikita7181@users.noreply.github.com>
2020-12-24 16:28:23 +0300

    Update Lb_11.cpp

--- Lb_11.cpp
+++ Lb_11.cpp
@@ -4,9 +4,7 @@
 #include <ctime>
 #include <mutex>
 #include <thread>
-#include <queue>
 #include <algorithm>
-#include <array>
 
 std::mutex vector_mtx;
 std::mutex cout_mtx;
@@ -22,11 +20,11 @@ std::vector<VectorIndex> generate_indexes(std::vector<int> vector, int thread_co
     std::vector<VectorIndex> result;
     int slice_size = 0;
     slice_size = vector.size() / thread_count;
+    VectorIndex vi;
     for (int i = 0; i < thread_count; i++) {
-        VectorIndex vi;
         vi.start = i * slice_size;
         vi.end = (i + 1) * slice_size;
-        if (i == (thread_count - 1)) 
+        if (i == (thread_count - 1))
         {
             vi.end = vector.size();
         }
@@ -35,10 +33,10 @@ std::vector<VectorIndex> generate_indexes(std::vector<int> vector, int thread_co
     return result;
 }
 
-std::vector<int> slicing (std::vector<int>& arr ,VectorIndex vi)// функция "разрезания" вектора
+std::vector<int> slicing(std::vector<int>& arr ,VectorIndex vi)// функция "разрезания" вектора
 {
     auto start = arr.begin() + vi.start;
-    auto end = arr.begin() + vi.end + 1;
+    auto end = arr.begin() + vi.end;
     std::vector<int> result(vi.end - vi.start);
     copy(start, end, result.begin());
     return result;
@@ -71,38 +69,39 @@ std::vector<int> generate_vector(int size)// функция заполнения
 
 int main()
 {
-    const int thread_count = 5;// измение кол-во потоков
+    const int thread_count = 24;// измение кол-во потоков
     int result_min_value;
     std::vector<int> vector;
     std::vector<VectorIndex> indexes;
-    std::thread* threads[thread_count];
+    std::thread *threads[thread_count];
     std::vector<int> results;
     std::vector<int> result_values[thread_count];
     std::chrono::steady_clock::time_point start_time;
     std::chrono::steady_clock::time_point stop_time;
     
-    vector = generate_vector(12600000);
+    vector = generate_vector(126000000);
     indexes = generate_indexes(vector, thread_count);
     
-    for (int i = 0; i < thread_count; i++) 
+    for (int i = 0; i < thread_count; i++)
     {
         results.push_back(-1);
     }
     
     start_time = std::chrono::steady_clock::now();
     
-    for (int i = 0; i < thread_count; i++) 
+    for (int i = 0; i < thread_count; i++)
     {
-        threads[i] = new std::thread(find_min, std::ref(vector), indexes[i], i, std::ref(results[i]));
+        threads[i] = new std::thread(find_min, std::ref(vector), indexes[i], i , std::ref(results[i]));
     }
     
-    for (auto & thread : threads) {
+    for (auto & thread : threads)
+    {
         thread -> join();
     }
     
     stop_time = std::chrono::steady_clock::now();
     
-    for (int i = 0; i < thread_count; i++) 
+    for (int i = 0; i < thread_count; i++)
     {
         result_values -> push_back(results[i]);
     }
@@ -110,8 +109,12 @@ int main()
     result_min_value = *std::min_element(result_values->begin(), result_values->end());
     std::cout << "result_min_value: " << result_min_value << std::endl;
     std::cout << "time: " << std::chrono::duration_cast<std::chrono::microseconds>(stop_time - start_time).count() << std::endl;
+    for (auto & thread : threads)
+    {
+       delete thread;
+    }
     return 0;
 }
 // В результате можно сказать, что много поток эффективен при 5 потоках
-// при одном потоке скорость 189231 мксек, а при 5 потоках 83224 мксек 
+// при одном потоке скорость 189231 мксек, а при 5 потоках 83224 мксек
 // самое оптимальное число эллементов 12600000+-500000
